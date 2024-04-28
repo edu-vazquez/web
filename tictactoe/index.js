ec79eb7592f801f092dc9878b5fbeb87e8d8415a
Eduardo Vazquez <you@example.com>
2024-04-28 16:39:48 +0200

    subida para probar el borde del grip que se visualiza en movil

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -8,30 +8,31 @@ document.getElementById("20").addEventListener("click", ejecutar);
 document.getElementById("21").addEventListener("click", ejecutar);
 document.getElementById("22").addEventListener("click", ejecutar);
 
+document.getElementById("anuncioBox").addEventListener("click", clean);
+
 let jugador = "X";
 let jugadasX = [];
 let jugadasO = [];
 document.getElementById("jugador").innerHTML = jugador;
 
 function ejecutar(e){
-    if (jugador == "X"){
+    let contenido = document.getElementById(e.target.id).textContent;
+    if (jugador == "X" && contenido == ""){
         document.getElementById(e.target.id).innerHTML = "X";
         jugadasX.push(e.target.id);
         jugador = "O";
         document.getElementById("jugador").innerHTML = "O";
         if (check(jugadasX)){
-            alert("ganador X");
-            clean();
+            anuncio("X");
         }
-    }else{
+    }else if (jugador == "O" && contenido == ""){
         document.getElementById(e.target.id).innerHTML = "O";
         jugador = "X";
         document.getElementById("jugador").innerHTML = "X";
         jugadasO.push(e.target.id);
         if (check(jugadasO)){
-            alert("ganador O");
-            clean();
-        };
+            anuncio("O");
+        }
     }
 }
 function check(array){
@@ -39,6 +40,8 @@ function check(array){
     let counter0 = 0;
     let counter1 = 0;
     let counter2 = 0;
+    let counterdig1 = 0;
+    let counterdig2 = 0;
 
     for (let j = 0; j <= 1 ; j++){
         for (let array_i = 0; array_i < array.length; array_i++){
@@ -52,17 +55,37 @@ function check(array){
                 case "2":
                     counter2++;
             }
+            switch (array[array_i]){
+                case "00":
+                    counterdig1++;
+                    break;
+                case "11":
+                    counterdig1++;
+                    counterdig2++;
+                    break;
+                case "22":
+                    counterdig1++;
+                    break;
+            }
+            switch (array[array_i]){
+                case "20":
+                    counterdig2++;
+                    break;
+                case "02":
+                    counterdig2++;
+                    break;
+            }
         }
-        if (counter0 == 3 || counter1 == 3 || counter2 == 3){
+        if (counter0 == 3 || counter1 == 3 || counter2 == 3 || counterdig1 == 3 || counterdig2 == 3){
             return (1);
         }else{
             counter0 = 0;
             counter1 = 0;
             counter2 = 0;
+            counterdig1 = 0;
         }
     }
     return(0);
-
 }
 function clean(){
     for (let i = 0; i <= 2; i++ ){
@@ -73,6 +96,9 @@ function clean(){
     }
     jugadasX = [];
     jugadasO = [];
-    console.log(jugadasX);
-    console.log(jugadasX);
+    document.getElementById("anuncioBox").style.display = "none";
+}
+function anuncio(winner){
+    document.getElementById("anuncio").innerHTML = `${winner}`;
+    document.getElementById("anuncioBox").style.display = "flex";
 }
