096413b5397d674a678f2acd2a47406eaa662363
Eduardo Vazquez <dev.eduardovazquez@gmail.com>
2025-05-22 22:05:25 +0200

    se cambia el cubo a una disposicion en espiral

--- src/components/CardContent.jsx
+++ src/components/CardContent.jsx
@@ -14,22 +14,22 @@ export default function CardContent(props){
   }
 
   const cubeRef = useRef(null)
-  const faceFront = useRef(null)
-  const faceBack = useRef(null)
-  const faceRight = useRef(null)
-  const faceLeft = useRef(null)
-  const faceTop = useRef(null)
-  const faceBottom = useRef(null)
+  const face01 = useRef(null)
+  const face02 = useRef(null)
+  const face03 = useRef(null)
+  const face04 = useRef(null)
+  const face05 = useRef(null)
+  const face06 = useRef(null)
 
   // Aplica la imagen de fondo cuando contentState cambia a "something"
   useEffect(() => {
-    if (contentState && faceFront.current) {
-      faceFront.current.style.backgroundImage = `url(${props.card.image01})`
-      faceBack.current.style.backgroundImage = `url(${props.card.image02})`
-      faceRight.current.style.backgroundImage = `url(${props.card.image03})`
-      faceLeft.current.style.backgroundImage = `url(${props.card.image04})`
-      faceTop.current.style.backgroundImage = `url(${props.card.image05})`
-      faceBottom.current.style.backgroundImage = `url(${props.card.image06})`
+    if (contentState) {
+      face01.current.style.backgroundImage = `url(${props.card.image01})`
+      face02.current.style.backgroundImage = `url(${props.card.image02})`
+      face03.current.style.backgroundImage = `url(${props.card.image03})`
+      face04.current.style.backgroundImage = `url(${props.card.image04})`
+      face05.current.style.backgroundImage = `url(${props.card.image05})`
+      face06.current.style.backgroundImage = `url(${props.card.image06})`
     }
   }, [contentState, props.card.image01])
 
@@ -42,12 +42,12 @@ export default function CardContent(props){
         contentState && (
           <div className="cube-container">
             <div class="cube" id="cube" ref={cubeRef}>
-              <div className="face front" id="face-front" ref={faceFront}>Front</div>
-              <div className="face back" id="face-back" ref={faceBack}>Back</div>
-              <div className="face right" id="face-right" ref={faceRight}>Right</div>
-              <div className="face left" id="face-left" ref={faceLeft}>Left</div>
-              <div className="face top" id="face-top" ref={faceTop}>Top</div>
-              <div className="face bottom" id="face-bottom" ref={faceBottom}>Bottom</div>
+              <div className="face face01" ref={face01}>Front</div>
+              <div className="face face02" ref={face02}>Back</div>
+              <div className="face face03" ref={face03}>Right</div>
+              <div className="face face04" ref={face04}>Left</div>
+              <div className="face face05" ref={face05}>Top</div>
+              <div className="face face06" ref={face06}>Bottom</div>
             </div>
           </div>
         )
