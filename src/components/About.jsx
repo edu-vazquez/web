b85a976a8be4bca7936dc61adfc4da27ac4d2e9c
Eduardo Vazquez <dev.eduardovazquez@gmail.com>
2025-06-24 16:46:30 +0200

    se agrega scale al headline, porque fontsize causa un rerender y un click al final de la animacion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,26 +23,20 @@ export default function About(){
     >
       <div className="about-container">
         <p>Hello!</p> 
-        <p>I’m a web developer based in Spain, passionate about creating innovative web solutions. I have a strong background in [mention relevant skills/technologies], and I love to experiment with new technologies to solve real-world problems.</p>
+        <p>I’m a web developer based in Spain, passionate about creating innovative web solutions. I have a strong background developing proyects  and I love to experiment with new technologies to solve problems.</p>
 
-      <ul>
-        <li>
-          <a href="https://github.com/edu-vazquez" target="_blank">
-            GitHub
-            <i class="fa-solid fa-up-right-from-square"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/eduvazquez/" target="_blank">
-            LinkedIn
-            <i class="fa-solid fa-up-right-from-square"></i>
-          </a>
-        </li>
-        <li className={'email selectable'}>
+        <a href="https://github.com/edu-vazquez" target="_blank">
+          GitHub
+          <i class="fa-solid fa-up-right-from-square"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/eduvazquez/" target="_blank">
+          LinkedIn
+          <i class="fa-solid fa-up-right-from-square"></i>
+        </a>
+        <p className={'email selectable'}>
             hola@eduardo-vazquez.com
             <i class="fa-solid fa-copy" id="btn-copy" onClick={copyMail}></i>
-        </li>
-      </ul>
+        </p>
       </div>
     </div>
 
